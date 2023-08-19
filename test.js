d3f8feab2c50ae720ea6cf50640be752b5725c38
ang92 <ang_92@list.ru>
2023-08-19 23:05:23 +0300

    2

--- test.js
+++ test.js
@@ -22,35 +22,27 @@ function guessNum() {
   }
   if (value == secretNum) {
     inputBox.innerHTML = `<p class="subtitle">Угадал за ${triesNum} ходов</p>
-      <button class="btnRepeat">Начать сначала</button>`;
+      <button class="btnRepeat" onclick="guessRepeat()">Начать сначала</button>`;
     underText.className = "hide";
   }
 }
 
+function guessRepeat() {
+  // Восстановление исходных условий для новой игры
+  secretNum = Math.ceil(Math.random() * 100);
+  console.log(secretNum);
+  triesNum = 1;
+  content.innerHTML = ` <h1 class="title">Угадай число</h1>
+            <p class="subtitle">Я загадал число от 1 до 100, ты должен угадать это число</p>
+            <div class="inputBox">
+                <input type="text" class="input">
+                <button class="btn">Угадать</button>
+            </div>
+            <p class="underText">—</p>
+            <p class="tries">Сыграно игр: 0</p>`;
+}
+
 btn.addEventListener("click", guessNum);
 
-content.addEventListener("click", function (event) {
-  if (event.target.classList.contains("btnRepeat")) {
-    gameNum++;
-    tries.innerHTML = `Сыграно игр: ${gameNum}`;
 
-    // Восстановление исходных условий для новой игры
-    secretNum = Math.ceil(Math.random() * 100);
-    console.log(secretNum);
-    triesNum = 1;
-    underText.textContent = "";
-    input.value = "";
-    let value = input.value;
-    if (value < secretNum) {
-      underText.textContent = "Больше";
-      triesNum++;
-    }
-    if (value > secretNum) {
-      underText.textContent = "Меньше";
-      triesNum++;
-    }
-    inputBox.innerHTML = `
-      <input type="text" class="input">
-      <button class="btn" onclick="guessNum()">Угадать</button>`;
-  }
-});
+
