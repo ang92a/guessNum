824836da9d02965cef47c24efd80d8c529c54f8b
ang92 <ang_92@list.ru>
2023-08-19 23:23:40 +0300

    3

--- test.js
+++ test.js
@@ -15,15 +15,13 @@ function guessNum() {
   if (value < secretNum) {
     underText.textContent = "Больше";
     triesNum++;
-  }
-  if (value > secretNum) {
+  } else if (value > secretNum) {
     underText.textContent = "Меньше";
     triesNum++;
-  }
-  if (value == secretNum) {
+  } else if (value == secretNum) {
     inputBox.innerHTML = `<p class="subtitle">Угадал за ${triesNum} ходов</p>
-      <button class="btnRepeat" onclick="guessRepeat()">Начать сначала</button>`;
-    underText.className = "hide";
+                <button class="btnRepeat">Начать сначала</button>`;
+    underText.classList.add("hide");
   }
 }
 
@@ -32,17 +30,26 @@ function guessRepeat() {
   secretNum = Math.ceil(Math.random() * 100);
   console.log(secretNum);
   triesNum = 1;
-  content.innerHTML = ` <h1 class="title">Угадай число</h1>
-            <p class="subtitle">Я загадал число от 1 до 100, ты должен угадать это число</p>
-            <div class="inputBox">
-                <input type="text" class="input">
-                <button class="btn">Угадать</button>
-            </div>
-            <p class="underText">—</p>
-            <p class="tries">Сыграно игр: 0</p>`;
+  content.innerHTML = `
+                <h1 class="title">Угадай число</h1>
+                <p class="subtitle">Я загадал число от 1 до 100, ты должен угадать это число</p>
+                <div class="inputBox">
+                    <input type="text" class="input">
+                    <button class="btn">Угадать</button>
+                </div>
+                <p class="underText">—</p>
+                <p class="tries">Сыграно игр: ${gameNum}</p>`;
+
+  // Добавляем обработчик для кнопки btn снова
+  btn.addEventListener("click", guessNum);
 }
 
 btn.addEventListener("click", guessNum);
 
-
-
+content.addEventListener("click", function (event) {
+  if (event.target.classList.contains("btnRepeat")) {
+    gameNum++;
+    tries.innerHTML = `Сыграно игр: ${gameNum}`;
+    guessRepeat();
+  }
+});
